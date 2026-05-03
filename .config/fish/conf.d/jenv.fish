b84e06201df4f40e9d211bfc6b26d98d2e36e6b3
Robert Tucker <robertwtucker@gmail.com>
2026-05-03 16:37:43 -0400

    fix: let `jenv` handle the init

--- .config/fish/conf.d/jenv.fish
+++ .config/fish/conf.d/jenv.fish
@@ -1,13 +1,4 @@
 # jenv initialization
 if status is-interactive
-    set -gx PATH '/Users/robert/.jenv/shims' $PATH
-    set -gx JENV_SHELL fish
-    set -gx JENV_LOADED 1
-    set -e JAVA_HOME
-    set -e JDK_HOME
-    if set -q HOMEBREW_PREFIX
-        source "$HOMEBREW_PREFIX/opt/jenv/completions/jenv.fish"
-    end
-    jenv rehash 2>/dev/null
-    jenv refresh-plugins
+    jenv init - fish | source
 end
